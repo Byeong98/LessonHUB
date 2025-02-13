656cdcc7e8e0095eb9d6b7ff66630024f58c18dc
김병민 <gimbyeongmin@byeong.local>
2025-02-14 04:07:02 +0900

    교수안 모델 작성 완료

--- backend/domain/teach/router.py
+++ backend/domain/teach/router.py
@@ -1,3 +1,4 @@
+import json
 from fastapi import APIRouter, HTTPException
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -19,13 +20,30 @@ router = APIRouter(
 async def teach_create(teach_create: schema.TeachCreate, db: AsyncSession = Depends(get_db)):
     completion = client.chat.completions.create(
         model="gpt-4o-mini",
+        response_format={"type": "json_object"},
         messages=[
-            {"role": "system", "content": "당신은 친절하고 정확한 교수안 작성 도우미입니다. 주어진 성취기준과 성취기준해설을 바탕으로 잘 구조화된 교수안을 작성해야 합니다. 교수안은 제목, 단원, 학습 목표, 도입, 전개, 정리로 구성됩니다."},
-            {"role": "user", "content": f"단원 :{teach_create.unit} \n 성취기준: {teach_create.standard} \n 성취기준해설: {teach_create.commentary}\n 성취기준과, 성취기준해설을 보고 교수안의 제목, 단원, 학습목표와 도입, 전개, 정리를 작성해줘."},
-            {"role": "assistant", "content": "제목, 단원\n 1. 학습 목표 설명\n2. 도입\n3. 전개\n4. 정리"}
-        ]
+            {
+                "role": "system",
+                "content": "당신은 친절하고 정확한 교수안 작성 도우미입니다. "
+                "주어진 성취기준과 성취기준 해설을 바탕으로 잘 구조화된 교수안을 작성해야 합니다. "
+                "교수안은 단원, 제목, 학습목표, 도입, 전개, 정리, 참고자료로 구성됩니다."
+                "You are a helpful assistant designed to output JSON."
+            },
+            {
+                "role": "user",
+                "content": f"단원: {teach_create.unit}\n"
+                f"성취기준: {teach_create.standard}\n"
+                f"성취기준해설: {teach_create.commentary}\n"
+                f"성취기준과 성취기준 해설을 보고 교수안을 작성해줘."
+                "{단원: string... , 제목: string..., 학습목표: [string ...], 도입: [string ...], 전개: [string ...], 정리: [string ...], 참고자료: [string + URL ...] } "
+            }
+        ],
     )
 
     result = completion.choices[0].message.content
-    return {"content": result}
+    if not result:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="교수안 제작 실패")
 
+    parsed_json = json.loads(result)
+    return parsed_json
