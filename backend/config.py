28f5919418f8ec55cb7739cb50cd2f9c65dc8632
김병민 <gimbyeongmin@byeong.local>
2025-02-17 03:51:04 +0900

    url 추가

--- backend/config.py
+++ backend/config.py
@@ -7,3 +7,6 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 1440))
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+HOST=os.getenv("HOST")
+PORT=os.getenv("PORT")
