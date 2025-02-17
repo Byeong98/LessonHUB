bac01c80f0c387a52ed19ab6c0f567c58a547ab2
김병민 <gimbyeongmin@byeong.local>
2025-02-17 21:26:37 +0900

    수정

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const baseURL = process.env.REACT_APP_BASE_URL;
 
 const api = axios.create({
-  baseURL: baseURL,
+  baseURL: "http://127.0.0.1:8000",
 });
 
 export default api;
