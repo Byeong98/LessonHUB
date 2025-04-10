fa6f479164bd981dd15f3b6ed24e075defbe1dc7
김병민 <gimbyeongmin@byeong.local>
2025-04-11 02:12:58 +0900

    github action 추가

---
+++ backend/test_main.py
@@ -0,0 +1,15 @@
+from fastapi.testclient import TestClient
+
+from main import app
+
+
+client = TestClient(app)
+    
+def test_teach_grades():
+    response = client.get("api/teach/grades")
+    assert response.status_code == 200
+    data = response.json()
+    assert isinstance(data, list)
+
+
+
